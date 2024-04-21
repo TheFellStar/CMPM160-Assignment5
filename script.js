1ea31352857c57bb69f99a9544c997b79d45a316
Ethan <122409712+TheFellStar@users.noreply.github.com>
2024-04-21 12:32:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from '/node_modules/three/build/three.module.js';
+import * as THREE from './node_modules/three/build/three.module.js';
 import {OBJLoader} from './node_modules/three/build/OBJLoader.js';
 import { MTLLoader } from './node_modules/three/build/MTLLoader.js';
 
